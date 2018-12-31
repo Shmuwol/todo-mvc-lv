71664c26429e50698cef7479d4dd2129e0ace538
Shmully Wolfson <shmullywolfson@gmail.com>
2018-12-31 13:30:00 -0500

    add pasword_digest

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,11 +6,13 @@ class UsersController < ApplicationController
   def create
     @user = User.new
     @user.email = params[:user][:email]
+    @user.password = params[:user][:password]
 
     if @user.save
+      session[:user_id] = @user.id
       redirect_to root_path
     else
-      render :new
+      render '/users/new'
     end
   end
 end
