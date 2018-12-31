d4ff2de327727fff2caf2484a5045b256246d4df
Shmully Wolfson <shmullywolfson@gmail.com>
2018-12-31 12:03:42 -0500

    adds user login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  helper_method :current_user
+
   def authentication_required
     if !logged_in?
       redirect_to login_path
@@ -10,6 +12,10 @@ class ApplicationController < ActionController::Base
   end
 
   def logged_in?
-    session[:user_id]
+    !!@current_user
+  end
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   get '/login' => "sessions#new"
   post '/sessions' => "sessions#create"
+  get '/logout' => "sessions#destroy"
 
   resources :users, only: [:new, :create]
-  
+
   resources :lists do
     resources :items
   end
